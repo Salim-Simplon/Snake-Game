c6a05922c3db2e6eb6d9d44d00ca9f98fca4408f
Amira-Chaabane <chaabaneamirasimplon@gmail.com>
2020-04-16 14:26:06 +0100

    game space creation

---
+++ snake_game.js
@@ -0,0 +1,15 @@
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// create the unit
+const box = 32;
+
+// load images
+
+const ground = new Image();
+ground.src =
+  "https://www.greengrasswater.com/wp-content/uploads/2017/06/coloration-gazon-1.jpg";
+
+const foodImg = new Image();
+foodImg.src =
+  "https://cdn2.iconfinder.com/data/icons/mammals-ii/300/10-512.png";
