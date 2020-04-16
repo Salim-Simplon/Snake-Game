280a3a3795e0282cbd8f60ce1bea3b69f534a5d1
Houssem AMMAR <houssem.ammar.simplon@gmail.com>
2020-04-16 14:42:51 +0100

    create food and direction function

--- snake_game.js
+++ snake_game.js
@@ -20,3 +20,37 @@ snake[0] = {
   x: 9 * box,
   y: 10 * box,
 };
+
+// create the food
+
+let food = {
+  x : Math.floor(Math.random()*17+1) * box,
+  y : Math.floor(Math.random()*15+3) * box
+}
+
+// create the score var
+
+let score = 0;
+
+//control the snake
+
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event){
+  let key = event.keyCode;
+  if( key == 37 && d != "RIGHT"){
+      
+      d = "LEFT";
+  }else if(key == 38 && d != "DOWN"){
+      d = "UP";
+     
+  }else if(key == 39 && d != "LEFT"){
+      d = "RIGHT";
+     
+  }else if(key == 40 && d != "UP"){
+      d = "DOWN";
+     
+  }
+}
