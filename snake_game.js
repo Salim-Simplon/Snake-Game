85fba9c34c00295f7472d4ab8c36d609dca243e0
Amira-Chaabane <chaabaneamirasimplon@gmail.com>
2020-04-16 14:48:53 +0100

    collision function

--- snake_game.js
+++ snake_game.js
@@ -24,9 +24,9 @@ snake[0] = {
 // create the food
 
 let food = {
-  x : Math.floor(Math.random()*17+1) * box,
-  y : Math.floor(Math.random()*15+3) * box
-}
+  x: Math.floor(Math.random() * 17 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 3) * box,
+};
 
 // create the score var
 
@@ -36,21 +36,26 @@ let score = 0;
 
 let d;
 
-document.addEventListener("keydown",direction);
+document.addEventListener("keydown", direction);
 
-function direction(event){
+function direction(event) {
   let key = event.keyCode;
-  if( key == 37 && d != "RIGHT"){
-      
-      d = "LEFT";
-  }else if(key == 38 && d != "DOWN"){
-      d = "UP";
-     
-  }else if(key == 39 && d != "LEFT"){
-      d = "RIGHT";
-     
-  }else if(key == 40 && d != "UP"){
-      d = "DOWN";
-     
+  if (key == 37 && d != "RIGHT") {
+    d = "LEFT";
+  } else if (key == 38 && d != "DOWN") {
+    d = "UP";
+  } else if (key == 39 && d != "LEFT") {
+    d = "RIGHT";
+  } else if (key == 40 && d != "UP") {
+    d = "DOWN";
   }
+}
+// cheack collision function
+function collision(head, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (head.x == array[i].x && head.y == array[i].y) {
+      return true;
+    }
+  }
+  return false;
 }
